082ad9ccb0214c9605ab6b6c65b931039c067fe5
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-12-17 23:55:06 -0500

    firebase connected

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCvPOti9v9JUo0tsJ3UXwj7MDegY5TpuYA",
+  authDomain: "challenge-36b47.firebaseapp.com",
+  projectId: "challenge-36b47",
+  storageBucket: "challenge-36b47.appspot.com",
+  messagingSenderId: "956739358448",
+  appId: "1:956739358448:web:9d6eab0504fcfa1c977c35",
+  measurementId: "G-X3EQRFNMT4",
+};
