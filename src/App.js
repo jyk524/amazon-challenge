48edf5f3354ffbd588dbfe9fae3d4b4d1612165e
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-12-23 00:03:55 -0500

    updated header in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     // BEM convention
     <div className="app">
-      <h1>App.js h1</h1>
-      {/* Header - search bar - profile/personal buttons */}
+      <Header />
       {/* Home */}
     </div>
   );
