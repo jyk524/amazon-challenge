7996e1d79b81afa664cb6dc5eef7f4a98ddb88c2
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-12-22 23:59:53 -0500

    header update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,7 +14,22 @@ function Header() {
         {/* Logo */}
       </div>
 
-      <div className="header__nav"></div>
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineOne">Sign In</span>
+        </div>
+
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineOne">& Orders</span>
+        </div>
+
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineOne">Prime</span>
+        </div>
+      </div>
     </div>
   );
 }
