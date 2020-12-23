09d239299ef3d0ec08243736dedf8b75b423cfb3
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-12-23 00:04:34 -0500

    updated header in app.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,17 +16,17 @@ function Header() {
 
       <div className="header__nav">
         <div className="header__option">
-          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineOne">Hello Guest </span>
           <span className="header__optionLineOne">Sign In</span>
         </div>
 
         <div className="header__option">
-          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineOne">Returns </span>
           <span className="header__optionLineOne">& Orders</span>
         </div>
 
         <div className="header__option">
-          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineOne">Your </span>
           <span className="header__optionLineOne">Prime</span>
         </div>
       </div>
