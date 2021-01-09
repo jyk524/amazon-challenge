79d9cd79e15629c5892235f34a1ad5ba2df951aa
jyk524 <67247717+jyk524@users.noreply.github.com>
2021-01-09 16:25:27 -0500

    Amazon Banner Image added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,13 @@ import "./Home.css";
 function Home() {
   return (
     <div className="home">
-      <p>Home Component</p>
+      <div className="home__container">
+        {/* Amazon Banner */}
+        <img
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt=""
+        />
+      </div>
     </div>
   );
 }
