645ba3221f3ac99a6803fce56c0c3551d09650a4
jyk524 <67247717+jyk524@users.noreply.github.com>
2021-01-08 22:30:14 -0500

    even spacing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -19,17 +19,17 @@ function Header() {
       <div className="header__nav">
         <div className="header__option">
           <span className="header__optionLineOne">Hello Guest </span>
-          <span className="header__optionLineTwo">Sign In</span>
+          <span className="header__optionLineTwo">Sign In </span>
         </div>
 
         <div className="header__option">
           <span className="header__optionLineOne">Returns </span>
-          <span className="header__optionLineTwo">& Orders</span>
+          <span className="header__optionLineTwo">& Orders </span>
         </div>
 
         <div className="header__option">
           <span className="header__optionLineOne">Your </span>
-          <span className="header__optionLineTwo">Prime</span>
+          <span className="header__optionLineTwo">Prime </span>
         </div>
         <div className="header__optionBasket">
           <ShoppingBasketIcon />
